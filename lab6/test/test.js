090d664bfd88a1ef8ff4d71c05cafe4b97fe643e
John Doe <you@example.com>
2024-10-22 18:18:30 +0300

    summary

--- lab6/test/test.js
+++ lab6/test/test.js
@@ -26,26 +26,37 @@ describe('POSTs', function () {
     it('/public/v2/users', function (done) {
         request('https://gorest.co.in')
             .post('/public/v2/users')
-            .send({ name: 'john' })
+            .send({
+                email: `test${Math.floor(Math.random() * 9999)}@test.com`,
+                name: 'testtestor',
+                gender: 'male',
+                status: 'inactive',
+              })
             .set('Accept', 'application/json')
+            .set('Authorization', 'Bearer d738c4fb8381ffb2ff86bdaf75957bc31b1c33200605a10a56dc2464546be9fe')
             .expect('Content-Type', /json/)
             .expect(200)
     });
     it('/public/v2/users/6942216/posts', function (done) {
         request('https://gorest.co.in')
             .post('/public/v2/users/7480809/posts')
-            .send({ body: 'post body' })
+            .send({
+                email: `test${Math.floor(Math.random() * 9999)}@test.com`,
+                name: 'testtestor',
+                gender: 'male',
+                status: 'inactive',
+              })
             .set('Accept', 'application/json')
-            .set('Authorization', 'Bearer d738c4fb8381ffb2ff86bdaf75957bc31b1c33200605a10a56dc2464546be9fe')
+            .send('Authorization', 'Bearer d738c4fb8381ffb2ff86bdaf75957bc31b1c33200605a10a56dc2464546be9fe')
             .expect('Content-Type', /json/)
             .expect(200)
     });
     it('/public/v2/users/7480808/todos', function (done) {
         request('https://gorest.co.in')
             .post('/public/v2/users/7480808/todos')
-            .send({ title: 'todo title', status: 'started' })
             .set('Accept', 'application/json')
-            .set('Authorization', 'd738c4fb8381ffb2ff86bdaf75957bc31b1c33200605a10a56dc2464546be9fe')
+            .set('Authorization', 'Bearer d738c4fb8381ffb2ff86bdaf75957bc31b1c33200605a10a56dc2464546be9fe')
+            .send({ title: 'todo title', status: 'started' })
             .expect('Content-Type', /json/)
             .expect(200, done)
     });
