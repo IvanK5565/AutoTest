da537b8e613262fb1b7a2e3e626f968dff19c496
John Doe <you@example.com>
2024-10-25 15:14:16 +0300

    lab6 complete

--- lab6/test/test.js
+++ lab6/test/test.js
@@ -1,25 +1,20 @@
-const supertest = require('supertest');
-const fs = require('fs')
-
-
-
+const request = require('supertest');
 
 describe('GETs', function () {
-    const request = supertest('https://gorest.co.in');
     it('/public/v2/users/7480808/posts', function (done) {
-        const res = request
+        const res = request('https://gorest.co.in')
             .get('/public/v2/users/7480808/posts')
             .expect('Content-Type', /json/)
             .expect(200, done)
     });
     it('/public/v2/users/7480808/comments', function (done) {
-        request
+        request('https://gorest.co.in')
             .get('/public/v2/posts/7480808/comments')
             .expect('Content-Type', /json/)
             .expect(200, done)
     });
     it('/public/v2/users/7480808/todos', function (done) {
-        request
+        request('https://gorest.co.in')
             .get('/public/v2/users/7480808/todos')
             .expect('Content-Type', /json/)
             .expect(200, done)
@@ -28,50 +23,54 @@ describe('GETs', function () {
 
 describe('POSTs', function () {
     const user1 = {
-        name: 'testtestor',
-        gender: 'male',
-        email: `test${Math.floor(Math.random() * 9999)}@test.com`,
-        status: 'active',
-      }
-    const request = supertest('https://gorest.co.in');
-    it('/public/v2/users', async function (done) {
-        const res = request
+        "name":"Ivan Kozlovsky",
+        "email":"example@mail.com",
+        "gender":"male",
+        "status":"active"
+    }
+    id=""
+    it('/public/v2/users', function (done) {
+        request('https://gorest.co.in')
             .post('/public/v2/users')
             .set('Authorization', 'Bearer d738c4fb8381ffb2ff86bdaf75957bc31b1c33200605a10a56dc2464546be9fe')
             .send(user1)
+            .expect(201)
+            .end((res)=>{
+                done()
+            })
+    });
+    it('/public/v2/users/6942216/posts', function (done) {
+        const post = {
+            "user_id": 7490836,
+            "title": "test post",
+            "body": "Post by Ivan Kozlovsky"
+        }
+        request('https://gorest.co.in')
+            .post("/public/v2/users/7490836/posts")
+            .set('Authorization', 'Bearer d738c4fb8381ffb2ff86bdaf75957bc31b1c33200605a10a56dc2464546be9fe')
+            .send(post)
             .expect('Content-Type', /json/)
-            .expect(200)
-            .done((res)=>{
-                expect(res.body).to.include(user1);
-                expect(res.body).to.have.property('id');
-                
+            .expect(201)
+            .end((res)=>{
+                done()
             })
         
-            expect(res.body).to.include(user1);
-            expect(res.body).to.have.property('id');
     });
-    it('/public/v2/users/6942216/posts', async function (done) {
-        const res = request
-            .post('/public/v2/users/7480809/posts')
+    it('/public/v2/users/7480808/todos', function (done) {
+        todo = {
+            "user_id": 7490836,
+            "title": "Todo by Ivan Kozlovsky",
+            "due_on": "2024-11-01T00:00:00.000+05:30",
+            "status": "pending"
+          },
+        request('https://gorest.co.in')
+            .post('/public/v2/users/7490836/todos')
             .set('Authorization', 'Bearer d738c4fb8381ffb2ff86bdaf75957bc31b1c33200605a10a56dc2464546be9fe')
-            .send(user1)
+            .send(todo)
             .expect('Content-Type', /json/)
-            .expect(200);
-        
-        expect(res.body).to.include(user1);
-        expect(res.body).to.have.property('id');
-    });
-    it('/public/v2/users/7480808/todos', async function (done) {
-        request
-            .post('/public/v2/users/7480808/todos')
-            .set('Accept', 'application/json')
-            .set('Authorization', 'Bearer d738c4fb8381ffb2ff86bdaf75957bc31b1c33200605a10a56dc2464546be9fe')
-            .send({ title: 'todo title', status: 'started' })
-            .expect('Content-Type', /json/)
-            .expect(200, done);
-
-        
-        expect(res.body).to.include(user1);
-        expect(res.body).to.have.property('id');
+            .expect(201)
+            .end((res)=>{
+                done()
+            })
     });
 });
